54347073f94d13ea5b5cf6e61cd45c23944f3235
waxiekkk <waxiekk@gmail.com>
2024-10-29 23:16:16 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function greetUser() {
+    const greeting = document.getElementById("greeting");
+    greeting.style.color = "#007acc";
+    greeting.style.fontSize = "1.2em";
+    greeting.style.marginTop = "20px";
+}
